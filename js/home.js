9b426402a3b2c1cd309a1e803184680215b39764
Shahedul Islam <shahedul872@gmail.com>
2023-05-17 13:00:26 +0600

    .

--- js/home.js
+++ js/home.js
@@ -1,5 +1,15 @@
-const swiper = new Swiper(".testimonials", {
-      slidesPerView: 3,
-      spaceBetween: 10,
-      freeMode: true,
+const swiper1 = new Swiper(".testimonials", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    freeMode: true,
+});
+const swiper2 = new Swiper(".home-slider", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    effect: "fade",
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
 });
